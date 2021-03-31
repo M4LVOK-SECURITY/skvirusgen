fd2c26148ca08b919bffd743fa694e4d79d64c87
SKELLING-OFICIAL <81347639+SKELLING-OFICIAL@users.noreply.github.com>
2021-03-31 14:18:20 -0600

    Update virusgen.sh

--- virusgen.sh
+++ virusgen.sh
@@ -3,7 +3,7 @@ clear
 chmod 777 Adware.sh
 chmod 777 Malware.sh
 chmod 777 RansomWare.sh
-chmod 777 VirusCreator.sh
+chmod 777 virusgen.sh
 echo "▄▄    ▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄    ▄▄    ▄▄▄▄       ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄   ▄▄" 
 echo "▀██  ██▀   ▀▀██▀▀   ██▀▀▀▀██  ██    ██  ▄█▀▀▀▀█    ██▀▀▀▀█  ██▀▀▀▀▀▀  ███   ██" 
 echo " ██  ██      ██     ██    ██  ██    ██  ██▄       ██        ██        ██▀█  ██" 
@@ -15,7 +15,7 @@ echo "=========="
 echo "IMPARTIENDO EL PODER DEL CONOCIMIENTO="
 echo "=========="
 echo " "
-echo "Creador de Virus Para Android:"
+echo "Virus Para Android:"
 echo "Opciones:"
 echo "1)Hadware.sh"
 echo "2)Malware.sh"
