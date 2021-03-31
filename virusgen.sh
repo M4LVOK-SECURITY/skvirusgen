cae2f736b9388eb390f712719e03c5b24a7d98eb
SKELLING-OFICIAL <81347639+SKELLING-OFICIAL@users.noreply.github.com>
2021-03-31 14:16:50 -0600

    Create virusgen.sh

---
+++ virusgen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+chmod 777 Adware.sh
+chmod 777 Malware.sh
+chmod 777 RansomWare.sh
+chmod 777 VirusCreator.sh
+echo "▄▄    ▄▄   ▄▄▄▄▄▄   ▄▄▄▄▄▄    ▄▄    ▄▄    ▄▄▄▄       ▄▄▄▄   ▄▄▄▄▄▄▄▄  ▄▄▄   ▄▄" 
+echo "▀██  ██▀   ▀▀██▀▀   ██▀▀▀▀██  ██    ██  ▄█▀▀▀▀█    ██▀▀▀▀█  ██▀▀▀▀▀▀  ███   ██" 
+echo " ██  ██      ██     ██    ██  ██    ██  ██▄       ██        ██        ██▀█  ██" 
+echo " ██  ██      ██     ███████   ██    ██   ▀████▄   ██  ▄▄▄▄  ███████   ██ ██ ██" 
+echo "  ████       ██     ██  ▀██▄  ██    ██       ▀██  ██  ▀▀██  ██        ██  █▄██" 
+echo "  ████     ▄▄██▄▄   ██    ██  ▀██▄▄██▀  █▄▄▄▄▄█▀   ██▄▄▄██  ██▄▄▄▄▄▄  ██   ███"
+echo "  ▀▀▀▀     ▀▀▀▀▀▀   ▀▀    ▀▀▀   ▀▀▀▀     ▀▀▀▀▀       ▀▀▀▀   ▀▀▀▀▀▀▀▀  ▀▀   ▀▀▀" 
+echo "=========="
+echo "IMPARTIENDO EL PODER DEL CONOCIMIENTO="
+echo "=========="
+echo " "
+echo "Creador de Virus Para Android:"
+echo "Opciones:"
+echo "1)Hadware.sh"
+echo "2)Malware.sh"
+echo "3)RansomWare.sh"
+read input
+bash $input
